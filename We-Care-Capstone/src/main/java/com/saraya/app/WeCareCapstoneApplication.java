91b861064314d5515dc3cb4335bdae74ed392ead
TidianiAmada <tidiani.amada@gmail.com>
2021-10-11 11:34:12 +0200

    initialization

---
+++ We-Care-Capstone/src/main/java/com/saraya/app/WeCareCapstoneApplication.java
@@ -0,0 +1,13 @@
+package com.saraya.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeCareCapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeCareCapstoneApplication.class, args);
+	}
+
+}
